7d913bf2e5fe5d7a88002715082ee7f4ca89b179
Arghya jain <arghyadprasad123@gmail.com>
2023-09-11 22:02:53 +0530

    some changes

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
-function sendMail(){
+function sendMail() {
     console.log(3);
-    var params = {
-        name : document.getElementById("form_name").value,
-        email : document.getElementById("form_email").value,
-        message : document.getElementById("get_text").value
+    var name = document.getElementById("form_name").value;
+    var email = document.getElementById("form_email").value;
+    var message = document.getElementById("get_text").value;
 
-    };
-    const service_id  = "service_53bmwad";
-    const template_id = "template_1yskd2f";
-    console.log(4);
-    emailjs.send(service_id, template_id, params)
-    .then(res => {
-        console.log(5);
-        document.getElementById("form_name").value = "";
-        document.getElementById("form_email").value = "";
-        document.getElementById("get_text").value = "";
-        console.log(6);
-        console.log(res);
-        alert("Your message sent successfully!");
-    })
-    .catch(err => {
-        console.log("oops");
-        console.error("Error sending email:", err);
-        alert("There was an error sending your message. Please try again later.");
-    });
+    if (name.trim() === '' || email.trim() === '' || message.trim() === '') {
+        alert("Please fill in all required fields.");
+    }
+    else {
+        var params = {
+            name: name,
+            email: email,
+            message: message
+
+        };
+        const service_id = "service_53bmwad";
+        const template_id = "template_1yskd2f";
+        console.log(4);
+        emailjs.send(service_id, template_id, params)
+            .then(res => {
+                console.log(5);
+                name = "";
+                email = "";
+                message = "";
+                console.log(6);
+                console.log(res);
+                alert("Your message sent successfully!");
+            })
+            .catch(err => {
+                console.log("oops");
+                console.error("Error sending email:", err);
+                alert("There was an error sending your message. Please try again later.");
+            });
+    }
 
 }
